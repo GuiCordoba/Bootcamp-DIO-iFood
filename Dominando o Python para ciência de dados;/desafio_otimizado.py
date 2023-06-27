0b901dbaa532971e3d2cf25f487f1c2da1de910a
Guilherme <guimcordoba@hotmail.com>
2023-06-27 20:21:53 -0300

    adicionado funções depositar, sacar e extrato

--- Dominando o Python para ciência de dados;/desafio_otimizado.py
+++ Dominando o Python para ciência de dados;/desafio_otimizado.py
@@ -18,9 +18,36 @@ def depositar(saldo, valor, extrato, /):
     if valor > 0:
         saldo = + valor
         extrato += f"Depósito: R${valor:.2f}\n"
-        print(">>>Operação realizada com sucesso<<<")
+        print("=== Operação realizada com sucesso ===")
     else:
-        print("!!!ERRO. Valor inválido!!!")
+        print("!!! ERRO. Valor inválido !!!")
+
+def sacar(*, saldo, valor, extrato, limite, numero_saque, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print(">>> Operação falhou! Saldo insuficiente <<<")
+
+    elif excedeu_limite:
+        print(">>> Operação falhou! Valor inserido excede o limite <<<")
+
+    elif excedeu_saques:
+        print(">>> Operação falhou! Número máximo de saques escedido <<<")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R${valor:.2f}\n"
+        numero_saques += 1
+    else:
+        print("!!!ERRO. Valor informado inválido!!!")
+
+def extrato(saldo, /, *, extrato):
+    print("\n------------EXTRATO------------")
+    print("Não foram realizados movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("-------------------------------")
 
 def main():
     LIMITE_SAQUES = 3
