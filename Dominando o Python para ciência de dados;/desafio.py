cdca45c16ef45b2e0eeff6b1b44ac3c237499fb4
Guilherme <guimcordoba@hotmail.com>
2023-06-27 17:08:57 -0300

    conclusão do projeto e adicionado uma opção de alterar limite de saque

--- Dominando o Python para ciência de dados;/desafio.py
+++ Dominando o Python para ciência de dados;/desafio.py
@@ -1,32 +1,72 @@
+
 menu = """
 Selecione uma opção:
 [1] Depositar;
 [2] Sacar;
 [3] Extrato;
-[4] Sair;
-"""
+[4] Alterar Limite de Saque;
+[5] Sair;
+-->"""
+
 saldo = 0
 limite = 500
 extrato = ""
 numero_saques = 0
-LIMITE_SAUQES = 3
+LIMITE_SAQUES = 3
 
 while True:
-    opcao = input(menu)
-     
+
+    opcao = int(input(menu))
+
     if opcao == 1:
-        print("Depósito")
+        valor = float(input("Insira o valor do depósito: "))
+
+        if valor > 0:
+            saldo = + valor
+            extrato += f"Depósito: R${valor:.2f}\n"
+        else:
+            print("ERRO! Valor inválido.")
 
     elif opcao == 2:
-        print("saque")
+        valor = float(input("Insira o valor para saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Saldo insuficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! Valor inserido excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques escedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R${valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("ERRO! Valor informado inválido.")
 
     elif opcao == 3:
-        print("Extrato")
+        print("\n------------EXTRATO------------")
+        print("Não foram realizados movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("-------------------------------")
 
     elif opcao == 4:
+        print(f"Limite atual: R${limite:.2f}")
+        novo_limite = float(input("Insira novo limite: R$"))
+        if novo_limite > 0:
+            limite = novo_limite
+        else:
+            print("ERRO! Valor informado inválido.")
+
+    elif opcao == 5:
         break
 
     else:
-        print("Opção inválida. Selecione um opção válida.")
+        print("Opção inválida! Selecione um opção válida.")
 
-print(menu)
